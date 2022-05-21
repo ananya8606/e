d35a298b8c13406fedc681c516989ea233b790ac
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-05-21 11:59:05 +0530

    Add files via upload

---
+++ backend/src/routes/authRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const auth = require("../middlewares/auth");
+
+const AuthController = require("../controllers/authController");
+
+// Is Human? Middleware
+
+const router = express.Router();
+
+router.post("/api/signin", AuthController.signIn);
+router.post("/api/signup", AuthController.signUp);
+router.post("/api/signout", auth, AuthController.signOut);
+
+module.exports = router;
---
+++ backend/src/routes/orderRoute.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+// Import auth middleware
+const auth = require("../middlewares/auth");
+
+// Import Admin auth middleware
+const adminAuth = require("../middlewares/adminAuth");
+
+// Import Order Controller
+const OrderController = require("../controllers/orderController");
+
+// Is Human? Middleware
+
+const router = express.Router();
+
+// User and Admin access only
+router.post("/api/order", auth,OrderController.addOrderItems);
+router.get("/api/order/:id", auth, OrderController.getOrder);
+router.delete("/api/order/:id", auth, OrderController.deleteOrder);
+
+// Admin access only
+// Get all the orders from the database
+router.get("/api/orders", auth, adminAuth, OrderController.getAllOrders);
+router.patch(
+    "/api/order/:id/deliver",
+    auth,
+    adminAuth,
+    OrderController.updateOrderToDelivered
+);
+
+module.exports = router;
